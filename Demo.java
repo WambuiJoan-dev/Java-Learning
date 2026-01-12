5a87f5d6f088a73d3d7d400995e9dc9d62dd9f26
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-12 17:28:28 +0300

    relational op

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args){
+        double x = 8.8;
+        double y = 9.8;
+
+        boolean result = x >= y;
+
+        System.out.println(result);
+    }
+}
