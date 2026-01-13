45be3f0c693b738a07f8271b82684b2f5b7cbb6e
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-13 21:02:01 +0300

    for loop

--- Loop.java
+++ Loop.java
@@ -2,15 +2,20 @@ public class Loop {
    public static void main(String a[])
     {
     
-    int i = 1;
-
-    while( i<=4 )
+     for (int i= 1; i <= 5; i++)
         
         {
-            System.out.println("Hi" + i);
+            System.out.println("DAY " + i);
+
+            for (int j = 1; j<=9; j++)
+
+            {
+                System.out.println("  "  + (j + 8));
+
+            }
         
-            i++;
-        }
-        System.out.println("Bye" + 1);
+            
+        } 
+        
     }
 }
