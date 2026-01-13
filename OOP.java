9d398fe577d8f5246ae2e2b66125edc841cb07b7
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-13 22:17:06 +0300

    objects

--- OOP.java
+++ OOP.java
@@ -3,9 +3,9 @@ class Calculator {
     
     int a;
 
-    public int add(){
-        System.out.println("in add");
-        return 0;
+    public int add(int n1, int n2){
+        int r = n1 + n2;
+        return r;
     }
 }
 
@@ -18,12 +18,12 @@ public class OOP {
         int num1 = 4;
         int num2 = 5;
 
-        Calculator calc = 
+        Calculator calc = new Calculator();
 
-        add();
+        int result = calc.add(num1, num2);
         
 
        // int result = num1 + num2;
-        //System.out.println(result);
+        System.out.println(result);
     }
 }
