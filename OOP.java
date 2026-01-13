7f4c6c9f700b466e24c730857fecbd985de92e06
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-13 22:04:30 +0300

    objects

---
+++ OOP.java
@@ -0,0 +1,29 @@
+
+class Calculator {
+    
+    int a;
+
+    public int add(){
+        System.out.println("in add");
+        return 0;
+    }
+}
+
+public class OOP {
+    //object oriented programming - objects and classes
+    //object- create a class first.
+    //JVM creates the object after you give it a class
+    public static void main(String a[])
+    {
+        int num1 = 4;
+        int num2 = 5;
+
+        Calculator calc = 
+
+        add();
+        
+
+       // int result = num1 + num2;
+        //System.out.println(result);
+    }
+}
