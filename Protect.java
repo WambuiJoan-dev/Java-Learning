c8288496344e6bcee23dde92635fb9d7b2695c59
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-13 19:41:33 +0300

    switch statement

--- Protect.java
+++ Protect.java
@@ -2,16 +2,17 @@ public class Protect {
     public static void main(String[] args) {
 
         String day = "Sunday";
+        String result = "";
 
-        switch(day)
+        result = switch(day)
         {
-            case "Saturday", "Sunday" -> System.out.println("6am");
+            case "Saturday", "Sunday" -> "6am";
                 
-            case "Monday" -> System.out.println("8am");
+            case "Monday" -> "8am";
                 
-            default -> System.out.println("7am");
+            default -> "7am";
 
             
-        }
+        };
     }
 }
