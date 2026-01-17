798bb993cd80adea46fbe50a20da9aadcde6dab0
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-17 18:04:52 +0300

    strings

--- Demo.java
+++ Demo.java
@@ -2,17 +2,25 @@ public class Demo
 {
     public static void main(String a[])
     {
-        int n = 1;
+        // int n = 1;
 
-        if (n ==1)
-            System.out.println("Monday");
-        else if (n==2)
-            System.out.println("Monday";);
-        System.out.println("Sunday");
-        System.out.println("Monday");
-        System.out.println("Tuesday");
-        System.out.println("Wednesday");
-        System.out.println("Thursday");
-        System.out.println("Friday");
+        // if (n ==1)
+        //     System.out.println("Monday");
+        // else if (n==2)
+        //     System.out.println("Monday";);
+        // System.out.println("Sunday");
+        // System.out.println("Monday");
+        // System.out.println("Tuesday");
+        // System.out.println("Wednesday");
+        // System.out.println("Thursday");
+        // System.out.println("Friday");
+
+        // String name  = new String("Joan");
+        String name = "Joan";
+        System.out.println("hello" + "," + name);
+        //System.out.println(name.charAt(1));
+        
+
+
     }
 }
