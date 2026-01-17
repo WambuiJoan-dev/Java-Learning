1595504e2665203cfd50cb78997c74ea28a27cf0
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-17 15:04:24 +0300

    arrays

--- array.java
+++ array.java
@@ -6,8 +6,12 @@ public static void main ( String a[])
         nums[0] = 4;
         nums[1] = 5;
 
-    //nums[1] = 6;
+    nums[3] = 6;
 
-    System.out.println(nums[1]);}
+    for(int i = 0; i<4; i++)
+
+    System.out.println(nums[i]);
+
+}
 
 }
