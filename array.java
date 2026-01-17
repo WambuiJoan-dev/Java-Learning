a9d9386651e1ee313989e7cfe1ad31f25b58bcbd
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-17 15:18:33 +0300

    arrays

--- array.java
+++ array.java
@@ -1,16 +1,28 @@
 public class Array {
     
-public static void main ( String a[])
+public static void main ( String a[]) {
 
-    {int nums[] = new int[4];
-        nums[0] = 4;
-        nums[1] = 5;
+    // {int nums[] = new int[4];
+    //     nums[0] = 4;
+    //     nums[1] = 5;
 
-    nums[3] = 6;
+    // nums[3] = 6;
 
-    for(int i = 0; i<4; i++)
+    // for(int i = 0; i<4; i++)
 
-    System.out.println(nums[i]);
+    // System.out.println(nums[i]);
+
+    int nums [] [] = new int [3] [4];
+    int random = (int) Math.random() * 100; 
+    for (int i=0; i<3; i++)
+    {
+        for (int j=0; j<4; j++)
+        {
+            System.out.print(nums[i][j] + " ");
+        }
+
+        System.out.println();
+    }
 
 }
 
