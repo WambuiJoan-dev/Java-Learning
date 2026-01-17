c34fe0386c0a3ebe77d1d826df6a3af35e92ef16
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-17 15:01:08 +0300

    arrays

---
+++ array.java
@@ -0,0 +1,13 @@
+public class Array {
+    
+public static void main ( String a[])
+
+    {int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 5;
+
+    //nums[1] = 6;
+
+    System.out.println(nums[1]);}
+
+}
