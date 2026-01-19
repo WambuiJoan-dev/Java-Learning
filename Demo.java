e65d685c242156d7066d7532ca8ee0e9342ebf2c
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-19 21:32:30 +0300

    this

--- Demo.java
+++ Demo.java
@@ -6,15 +6,15 @@ class Human
     public int getAge()
     {return age;}
 
-    public void setAge(int a)
-    {age = a;}
+    public void setAge(int age)
+    {this.age= age;}
 
 
     public String getName()
     {return name;}
 
-    public void setName(String n)
-    {name= n;}
+    public void setName(String name)
+    {this.name= name;}
 
 }
 
