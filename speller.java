b7d79d8d47eedf58ecf858b5cbf7d02ea6e0c811
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-01-27 05:20:00 +0300

    forloop

---
+++ speller.java
@@ -0,0 +1,9 @@
+public class speller {
+    public static void main(String[] args){
+        String toSpell = "conscientious";
+        int wordLength = toSpell.length();
+        for (int i = 0; i < wordLength; i++){
+            System.out.println(toSpell.charAt(i));
+        }
+    }
+}
