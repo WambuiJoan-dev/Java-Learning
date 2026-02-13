2a41d855e67066ec35ad940879b9a9648191e252
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-13 06:02:27 +0300

    methods

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-public class Main {
-    final int x=10 ;// use final to remove the ability to override attributes
-//overriding existing values
-    public static void main(String[] args) {
-        Main myObj = new Main();
-        myObj.x = 25;
-        System.out.println(myObj.x);
+public class Main{
+    static void myMethod(String fname) {
+        System.out.println(fname + " Joan");
+    }
+    
+    public static void main(String[] args){
+        myMethod("Ann");
+        myMethod("Jenny");
     }
 }
