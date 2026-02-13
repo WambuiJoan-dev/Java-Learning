643b7219ec6b12112827ab8631f917bde423d814
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-13 05:31:27 +0300

    attributes

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-    int x = 5;
-
+    int x=10 ;
+//overriding existing values
     public static void main(String[] args) {
         Main myObj = new Main();
+        myObj.x = 25;
         System.out.println(myObj.x);
     }
 }
