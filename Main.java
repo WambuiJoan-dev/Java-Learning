15c1983527d3472a01c3dea675221aec6f132218
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-13 15:28:39 +0300

    methods

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main{
-    static void myMethod(String fname) {
-        System.out.println(fname + " Joan");
-    }
-    
-    public static void main(String[] args){
-        myMethod("Ann");
-        myMethod("Jenny");
-    }
+     //create a checkage() method with an integer variable
+     static void checkAge(int age) {
+        //if age is less that 18 print "access denied"
+        if (age < 18){
+            System.out.println("Access denied -  You are too young");
+        }
+        //if age is greater than or equal to 18 print access granted
+        else {
+            System.out.println("Access granted - You are old enough");
+        }
+     }
+     public static void main(String[] args) {
+        checkAge(17);
+     }
 }
