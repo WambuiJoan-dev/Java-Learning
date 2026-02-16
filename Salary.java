78ac50a84aea76106f747a416c86e6ee80b8ece8
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-16 06:30:52 +0300

    methods

---
+++ Salary.java
@@ -0,0 +1,9 @@
+public class Salary {
+    public void earn(int amount){
+        System.out.println("its payday");
+    }
+    public static void main(String[] args){
+        Salary myObj = new Salary();
+        
+    }
+}
