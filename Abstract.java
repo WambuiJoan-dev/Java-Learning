17b3889f144bddfe67409ad16291766853ea2fef
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-18 05:44:22 +0300

    abstract

--- Abstract.java
+++ Abstract.java
@@ -7,11 +7,17 @@ class Dog extends Animal{
         System.out.println("Bark");
     }
 }
-
+class Cat extends Animal {
+    void makeSound(){
+        System.out.println("Meow");
+    }
+}
 
 public class Abstract {
     public static void main(String[] args) {
         Dog d = new Dog();
         d.makeSound();
+        Cat c = new Cat();
+        c.makeSound();
     }
 }
