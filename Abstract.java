06dad2d16c7007a3f8e2ac00f8006db7142995d5
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-18 05:35:33 +0300

    abstract

---
+++ Abstract.java
@@ -0,0 +1,10 @@
+public String fname = "Joan";
+public int age = 24;
+public abstract void study();
+
+class Student extends Main {
+    public int graduationYear = 2024;
+    public void study() {
+        System.out.println("...");
+    }
+}
