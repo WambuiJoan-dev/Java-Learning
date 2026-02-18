2dfd05e289e53193875f3d78988f13ded8e04df9
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-18 05:58:18 +0300

    encapsulation

--- Person.java
+++ Person.java
@@ -1,3 +1,12 @@
 public class Person { //encapsulation- make sure sensitive info is hidden from users
     private String name;
+
+    //Getter
+    public String getName(){
+        return name;
+    }
+    //Setter
+    public String setName(){
+        this.name = newName;
+    }
 }
