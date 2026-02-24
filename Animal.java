13dcd86359ba5a09fe42710af1a2ff6e5e80e0ae
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-24 08:43:13 +0300

    polymorphism

--- Animal.java
+++ Animal.java
@@ -1,15 +1,15 @@
 //inheritance concept
-class Animal {
+class Animal { //superclass-to be inherited from
     public void animalSound() {
         System.out.println("The animal makes a sound");
     }
 }
-class Pig extends Animal{
+class Pig extends Animal{//subclass inherits from superclass
     public void animalSound(){
         System.out.println("The pig says: wee wee");
     }
 }
-class Dog extends Animal{
+class Dog extends Animal{//subclass inherits from superclass
     public void animalSound(){
         System.out.println("The dog says: bow bow");
     }
@@ -19,7 +19,7 @@ class Main{
     public static void main(String[] args){
         Animal myAnimal = new Animal();//create a Animal object
         Animal myPig = new Pig();//create a pig object
-        Animal myDog = new Dog();
+        Animal myDog = new Dog();//create a Dog object
         myAnimal.animalSound();
         myDog.animalSound();
         myPig.animalSound();
