831ea1e82acedf6f2f1054e7f3adeac5b65f7bd3
WambuiJoan-dev <wambuijoan44@gmail.com>
2026-02-24 05:30:03 +0300

    inheritance

--- vehicle.java
+++ vehicle.java
@@ -4,3 +4,16 @@ class vehicle {
         System.out.println("Tuut, tuut!");
     }
 }
+
+class Car extends vehicle {
+    private String modelName = "Mustang";
+    public static void main(String[] args){
+        //create a myCar object
+        Car myCar = new Car();
+
+        //call the honk method from the vehicle class on the myCar object
+        myCar.honk();
+        //display the value of the brand attribute(from the Vehicle class) and the value of the modelName from the Car class
+        System.out.print(myCar.brand + " " + myCar.modelName);
+    }
+}
